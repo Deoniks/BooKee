ea2a7789f90fa48232dd254f32b5456021d840ea
Chaos Creator <CreaterChaos@outlook.com>
2020-10-02 17:21:20 +0600

    Test#One

---
+++ BooKee/Service/dbSave.cs
@@ -0,0 +1,46 @@
+﻿using BooKee.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooKee.Service
+{
+    class dbSave
+    {
+        private readonly string PATH;
+
+        public dbSave(string path)
+        {
+            PATH = path;
+        }
+        public BindingList<TodoMod> LoadDate()
+        {
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<TodoMod>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<TodoMod>>(fileText);
+            }
+        }
+
+        public void SaveDate(object todoData)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+
+                string output = JsonConvert.SerializeObject(todoData);
+                writer.Write(output);
+            }
+        }
+    }
+}
---
+++ BooKee/Task.xaml.cs
@@ -0,0 +1,70 @@
+﻿using BooKee.Models;
+using BooKee.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BooKee
+{
+    /// <summary>
+    /// Логика взаимодействия для Task.xaml
+    /// </summary>
+    public partial class Task : Window
+    {
+        private BindingList<TodoMod> _todoData;
+        private dbSave dbSave;
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoData.json";
+
+        public Task()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dbSave = new dbSave(PATH);
+
+            try
+            {
+                _todoData = dbSave.LoadDate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+
+            dgTodoList.ItemsSource = _todoData;
+            dgTodoListTwo.ItemsSource = _todoData;
+            _todoData.ListChanged += _todoData_ListChanged;
+        }
+
+        private void _todoData_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if(e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemChanged || e.ListChangedType == ListChangedType.ItemDeleted)
+            {
+                try
+                {
+                    dbSave.SaveDate(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
+            
+        }
+    }
+}
